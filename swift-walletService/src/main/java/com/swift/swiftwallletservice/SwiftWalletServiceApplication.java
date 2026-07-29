c4f92c38a04bdbdee3a986faf54a162cb7ffbc34
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-07-29 02:57:13 -0700

    refactor: decouple all services from parent POM and standardize Dockerfiles and deploy workflows

--- swift-walletService/src/main/java/com/swift/swiftwallletservice/SwiftWalletServiceApplication.java
+++ swift-walletService/src/main/java/com/swift/swiftwallletservice/SwiftWalletServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwiftWallletServiceApplication {
+public class SwiftWalletServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwiftWallletServiceApplication.class, args);
+        SpringApplication.run(SwiftWalletServiceApplication.class, args);
     }
 
 }
