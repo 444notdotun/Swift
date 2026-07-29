e530cb3c9b8c54991ddba437753cae585976948e
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-07-28 20:58:09 -0700

    refactor: decouple swift-userService from parent POM and update Dockerfile for simplified build

--- swift-userService/src/main/java/com/swift/swiftuserservice/SwiftUserServiceApplication.java
+++ swift-userService/src/main/java/com/swift/swiftuserservice/SwiftUserServiceApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.swift.swiftuserservice;
+package com.swift.swiftuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
